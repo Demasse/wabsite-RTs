f3511caa68ad5d0d07b31dbbeb96c24c4acc6bd1
Demasse <demassedimas2006@gmail.com>
2025-02-16 23:22:39 +0100

    ini & begining

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className='  bgb'>
+
+<Navbar />
+
+      <h1 className='text-red-500 text'>jhjhg</h1>
+      
+
+    </div>
+  )
+}
+
+export default App
