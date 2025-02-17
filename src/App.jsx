e984bdec36168869e61ea23abf22f5ab94ff8bbb
Demasse <demassedimas2006@gmail.com>
2025-02-17 13:51:36 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './components/Hero'
 import Analytics from './components/Analytics'
 import Newsletter from './components/Newsletter'
 import { Cards } from './components/Cards'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
 <Analytics />
 <Newsletter />
 <Cards />
+<Footer />
 
       {/* <h1 className='text-red-500 text'>jhjhg</h1> */}
       
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {FaDribbbleSquare,
+    FaFacebookSquare,
+    FaGithubSquare,
+    FaInstagram,
+    FaTwitterSquare,  } from 'react-icons';
+
+ const Footer = () => {
+  return (
+    <div className=' max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3
+     gap-8 text-gray-300  '>
+        <div className="   ">
+        <h1 className=" w-full text-3xl font-bold text-[#00df9a]  "> Dann〽️</h1>
+<p>  Centralisez vos analyses de données pour une meilleure visibilité, des décisions éclairées et une gestion efficace. Optimisez votre stratégie </p>
+        </div>
+     </div>
+  )
+}
+export default Footer
